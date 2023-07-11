e5f2d53c3342a3aeeb455dddddb7f3cecfdfa735
BaconBao <baconbao@users.noreply.github.com>
2023-07-11 08:47:02 +0800

    fix: eslint issue

--- pages/_document.js
+++ pages/_document.js
@@ -20,9 +20,9 @@ class MyDocument extends Document {
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
                     <meta name="author" content="BaconBao" />
                     <meta name="google-site-verification" content="uEUOEZWK4hbuXKG4fN2BseuNSZ3h5L7Mu__O2PX2dpY" />
-                    <link rel="alternate" hreflang="en" href="https://baconbao.github.io/en-us/" />
-                    <link rel="alternate" hreflang="zh" href="https://baconbao.github.io" />
-                    <link rel="alternate" hreflang="x-default" href="https://baconbao.github.io" />
+                    <link rel="alternate" hrefLang="en" href="https://baconbao.github.io/en-us/" />
+                    <link rel="alternate" hrefLang="zh" href="https://baconbao.github.io" />
+                    <link rel="alternate" hrefLang="x-default" href="https://baconbao.github.io" />
                 </Head>
                 <body>
                     <Main />
